3fad1ebbe991fb73e4ea4b7b4b28f2a2b7aa1329
Tobias Markmann <tm@ayena.de>
2012-04-07 23:51:10 +0200

    Made a Maven project out of it. Added basic test for caesar using QuickCheck.

---
+++ src/test/java/itsp/KryptoTest.java
@@ -0,0 +1,30 @@
+package itsp;
+
+import static net.java.quickcheck.generator.CombinedGeneratorsIterables.someLists;
+import static net.java.quickcheck.generator.PrimitiveGenerators.integers;
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import net.java.quickcheck.generator.PrimitiveGenerators;
+import net.java.quickcheck.generator.iterable.Iterables;
+import org.testng.annotations.Test;
+
+public class KryptoTest {
+
+    @Test
+    public void CaesarTest() {
+        for (Integer key : Iterables.toIterable(PrimitiveGenerators.integers(0, 255))) {
+            for (String cleartext : Iterables.toIterable(PrimitiveGenerators.strings())) {
+                // encrypt
+                String cyphertext = Krypto.cesar(cleartext, key, true);
+
+                // decrypt
+                String result = Krypto.cesar(cyphertext, key, false);
+                assertEquals(cleartext, result);
+            }
+        }
+    }
+}
