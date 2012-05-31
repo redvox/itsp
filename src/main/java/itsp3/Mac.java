2e7676c6a41e47a388a69b518b66331d5d8c57db
redvox <redpaperfox@gmail.com>
2012-05-31 18:18:42 +0200

    Added method to convert hex string into byte array.

--- src/main/java/itsp3/Mac.java
+++ src/main/java/itsp3/Mac.java
@@ -8,64 +8,86 @@ public class Mac {
 
 	public static void main(String[] args) {
 
-		p(secretPreMac("0000000000000000", "/home/oldvox/Dropbox/Code/itsp/text.txt"));
-		p(secretPreMac("0101111100000000", "/home/oldvox/Dropbox/Code/itsp/text.txt"));
-		p(secretPreMac("1111111111111111", "/home/oldvox/Dropbox/Code/itsp/text.txt"));
+		p(secretPreMac("00000000000000000000000000000000",
+				"/home/oldvox/Dropbox/Code/itsp/text.txt"));
+		p(secretPreMac("00000000000000000000000000000000",
+				"/home/oldvox/Dropbox/Code/itsp/text.txt"));
+		p(secretPreMac("00000000000000000000000000000000",
+				"/home/oldvox/Dropbox/Code/itsp/text.txt"));
 	}
 
-	public static String secretPreMac(String hexkey, String filename) {	
+	public static String secretPreMac(String hexkey, String filename) {
 		StringBuffer intext = new StringBuffer();
-		
 		try {
-		    BufferedReader in = new BufferedReader(new FileReader(filename));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		        intext.append(str);
-		    }
-		    in.close();
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String str;
+			while ((str = in.readLine()) != null) {
+				intext.append(str);
+			}
+			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		int padding = intext.length() % 16;
-		p("Padding: "+padding);
-		while(padding > 0){
+		p("Padding: " + padding);
+		while (padding > 0) {
 			intext.append("0");
 			padding--;
 		}
-		
+
 		// Secret Präfix? Der Inizialvekor könnte dann der Schlüssel sein.
-//		String initVector = "0000000000000000";
-		String initVector = hexkey;
+		// String initVector = "0000000000000000";
 		int macRounds = intext.length() / 16;
-		p("Rounds: "+macRounds);
-		
+		p("Rounds: " + macRounds);
+
 		byte[] intextArray = intext.toString().getBytes();
-		byte[] initVectorArray = initVector.getBytes(); 
+		byte[] initVectorArray = toByteArray(hexkey);
+		p("Lange "+initVectorArray.length);
 		byte[] tmp = new byte[16];
-		
-		for(int j=0; j < 16; j++){
+
+		for (int j = 0; j < 16; j++) {
 			tmp[j] = (byte) (intextArray[j] ^ initVectorArray[j]);
-		}		
-		
-		for(int i=0; i <= macRounds; i++){
-			for(int k=0; k < 16; k++){
-				tmp[k] = (byte) (tmp[k] ^ intextArray[(i*16)+k]);
+		}
+
+		for (int i = 0; i <= macRounds; i++) {
+			for (int k = 0; k < 16; k++) {
+				tmp[k] = (byte) (tmp[k] ^ intextArray[(i * 16) + k]);
 			}
 		}
-		
+
 		// finanisieren?
-		
-//		return new String(tmp);
-		
+
+		// return new String(tmp);
+
 		StringBuffer mac = new StringBuffer();
-		for(int j=0; j < 16; j++){
+		for (int j = 0; j < 16; j++) {
 			mac.append(String.valueOf(tmp[j]));
 		}
 		return mac.toString();
 	}
-	
-	public static void p(String s){
+
+	public static void macBruteForce(int leadingZeros, String filename,
+			String macValue) {
+		// Ausgabe
+		// Anzahl möglicher Schlüssel
+		// Anzahl getesteter Schlüssel
+		// Geheimer Schlüssel in Hexadezimalformat
+		// Laufzeit
+
+	}
+
+	// Max ist 7F (127).
+	public static byte[] toByteArray(String hexString) {
+		int numberChars = hexString.length();
+		byte[] bytes = new byte[numberChars / 2];
+		for (int i = 0; i < numberChars-1; i += 2) {
+			bytes[i / 2] = Byte.parseByte(hexString.substring(i, i+2), 16);
+		}
+		return bytes;
+	}
+
+	public static void p(String s) {
 		System.out.println(s);
 	}
 }
